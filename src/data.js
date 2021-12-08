878d152ad91e091d1a8ecaf4a894f4a9317d6602
ayofimihan <champion@Champions-MacBook-Pro.local>
2021-12-08 15:12:33 +0100

    playing with props

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Ogunsina Champion',
+    age: 22,
+    image:
+      'https://res.cloudinary.com/godfimihan/image/upload/v1638961066/861EF553-EAC9-4061-96E6-D64DA2378B5D_whf1au.jpg',
+  },
+  {
+    id: 5,
+    name: 'Will smith',
+    age: 53,
+    image:
+      'https://bloximages.chicago2.vip.townnews.com/madison.com/content/tncms/assets/v3/editorial/6/01/60178178-c90b-5793-9f81-e5a5046d3129/614b41d3f096b.image.jpg?resize=1476%2C1034',
+  },
+  {
+    id: 2,
+    name: 'Donald Glover',
+    age: 38,
+    image:
+      'https://yt3.ggpht.com/ytc/AKedOLR54qbXPUrgKCf0tmiquuqTiYDElO_Cekw-pSCs_g=s900-c-k-c0x00ffffff-no-rj',
+  },
+  {
+    id: 4,
+    name: 'T.I',
+    age: 41,
+    image:
+      'https://bloximages.chicago2.vip.townnews.com/madison.com/content/tncms/assets/v3/editorial/5/00/5001dd82-7d4f-5158-95c0-c53c2e5094fe/614b41d31363c.image.jpg?resize=1476%2C984',
+  },
+  {
+    id: 3,
+    name: 'Scottie Pippen',
+    age: 56,
+    image:
+      'https://bloximages.chicago2.vip.townnews.com/madison.com/content/tncms/assets/v3/editorial/f/89/f8988b8c-c527-5718-b893-d2dd99f9c776/614b41d29867c.image.jpg?resize=1476%2C981',
+  },
+]
